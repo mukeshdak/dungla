695250a4fb3b80d50772d5589cf9f95fec7e5f22
Mukesh Dak <mukeshdak@gmail.com>
2022-02-06 00:55:05 +0530

    11
    22

--- demo/javascript/form-validation/script.js
+++ demo/javascript/form-validation/script.js
@@ -1,22 +1,22 @@
 // Javascript for form inputs validation
 
 function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email.toLowerCase());
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email.toLowerCase());
 }
 
 function validate_form() {
-    var username = document.getElementById("username").value;
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-    confirm_password = document.getElementById("confirm_password").value;
+  alert("hi");
+  var username = document.getElementById("username").value;
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  confirm_password = document.getElementById("confirm_password").value;
 
-    if (username.length < 3) {
-        alert("Username must be at least three characters long.");
-    }
+  if (username.length < 3) {
+    alert("Username must be at least three characters long.");
+  }
 
-    if (validateEmail(email) != true) {
-        alert("email entered is invalid");                
-    }
-
+  if (validateEmail(email) != true) {
+    alert("email entered is invalid");
+  }
 }
